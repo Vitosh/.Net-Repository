4c379a50256cbd3c6da5fba02a789424e060109e
Vitosh Academy <vitosh.doynov@gmail.com>
2015-11-23 00:06:20 +0100

    Create notify_icon.vb

---
+++ notify_icon.vb
@@ -0,0 +1,33 @@
+
+
+Public Class Form1
+
+    Private Sub NotifyIcon1_MouseDown(sender As Object, e As MouseEventArgs) Handles NotifyIcon1.MouseDoubleClick
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            Using my_form As New Form1()
+                my_form.ShowDialog()
+            End Using
+        End If
+    End Sub
+
+    Private Sub StartToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles StartToolStripMenuItem.Click
+
+        Dim str_lorem As String = "lorem ipsum taratatatatatata lorem ipsum" & _
+            vbCrLf & "more lore ipsum here!" & _
+            vbCrLf & "and even more lorem ipsum there."
+
+        NotifyIcon1.ShowBalloonTip(1000, "Welcome    ", str_lorem, ToolTipIcon.Info)
+    End Sub
+
+    Private Sub EndToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles EndToolStripMenuItem.Click
+        Close()
+    End Sub
+
+    Private Sub ContextMenuStrip1_Opening(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles ContextMenuStrip1.Opening
+
+    End Sub
+
+    Private Sub AleToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AleToolStripMenuItem.Click
+        MsgBox("Opa")
+    End Sub
+End Class
